3ebf64dd863ce57d248a799f9ae68e80285f6c2b
Gryphonclaw111 <31829082+Gryphonclaw111@users.noreply.github.com>
2018-03-18 09:36:28 -0700

    Create initials.c
    Finds a person's initials

---
+++ initials.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void)
+{   
+    string s = GetString();
+    
+    printf("%c", toupper(s[0]));
+    
+    for (int i = 0, n = strlen(s); i < n; i++)
+    {
+       if (s[i] == 32)
+       {
+           printf("%c", toupper(s[i + 1]));
+       }
+       else
+       {
+           printf("");
+       }
+    }
+    
+    printf ("\n");
+}   
